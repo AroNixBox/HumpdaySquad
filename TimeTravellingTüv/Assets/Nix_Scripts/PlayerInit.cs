f69802413cce9e6a6de372e3a1f2e298e9482e1f
Nixon Scherbarth <nixonbusinessmail@gmail.com>
2024-03-02 01:37:43 +0100

    ADDED: VFX

--- TimeTravellingTüv/Assets/Nix_Scripts/PlayerInit.cs
+++ TimeTravellingTüv/Assets/Nix_Scripts/PlayerInit.cs
@@ -3,23 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using StarterAssets;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerInit : MonoBehaviour
 {
     [SerializeField] private Transform playerCapsuleTransform;
     [SerializeField] private Transform playerCameraRootTransform;
     [SerializeField] private FirstPersonController firstPersonController;
+    [SerializeField] private ParticleSystem tpVFX;
+    private CharacterController _characterController;
     private void Start()
     {
+        _characterController = GetComponent<CharacterController>();
         LoadPlayerStateAndSetPosition();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F5))
-        {
-            SceneManager.Instance.ChangeScene(playerCapsuleTransform.position, playerCameraRootTransform.rotation, playerCapsuleTransform.rotation);
-        }
+        if (!Input.GetKeyDown(KeyCode.F5)){ return; }
+        if(!firstPersonController.Grounded) { return; }
+        
+        tpVFX.Play();
+        SceneManager.Instance.ChangeScene(playerCapsuleTransform.position, playerCameraRootTransform.rotation, playerCapsuleTransform.rotation);
     }
 
     private void LoadPlayerStateAndSetPosition()
@@ -29,7 +34,8 @@ public class PlayerInit : MonoBehaviour
         //Nothing to load
         if(playerCapsulePosition == Vector3.zero && cameraRootRotation == Quaternion.identity && playerCapsuleRotation == Quaternion.identity) { return; }
 
-        GetComponent<CharacterController>().Move(playerCapsulePosition);
+        _characterController.Move(playerCapsulePosition);
         firstPersonController.InitializeCameraRotation(cameraRootRotation, playerCapsuleRotation);
+        tpVFX.Play();
     }
 }

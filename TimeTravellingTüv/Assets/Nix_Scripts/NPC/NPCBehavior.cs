2ab32cf3257e293d7654808d2307ab3c356cadd1
Nixon Scherbarth <nixonbusinessmail@gmail.com>
2024-03-03 13:58:30 +0100

    ADDED: Animations and Sounds to NPC

--- TimeTravellingTüv/Assets/Nix_Scripts/NPC/NPCBehavior.cs
+++ TimeTravellingTüv/Assets/Nix_Scripts/NPC/NPCBehavior.cs
@@ -3,29 +3,42 @@ using System.Collections;
 using Nix_Scripts.SceneSwitch;
 using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class NPCBehavior : MonoBehaviour, ITalkable
 {
+    [Header("References")]
+    [SerializeField] private AudioClip[] talkClips;
     [SerializeField] private TextMeshProUGUI chatBubbleTMP;
     [SerializeField] private GameObject chatParentImage;
-    private int _currentChatIndex;
+    
+    [Header("Values")]
     [SerializeField, TextArea(3, 10)] private string[] chat;
+    private int _currentChatIndex;
     private NPCHeadLookAt _npcHeadLookAt;
     
     private Coroutine _typingCoroutine;
     private bool _isTyping;
 
+    private AudioSource _audioSource;
+    private Animator _animator;
+    private static readonly int TalkAnim_1 = Animator.StringToHash("Talk_1");
+    private static readonly int TalkAnim_2 = Animator.StringToHash("Talk_2");
+    private static readonly int TalkAnim_3 = Animator.StringToHash("Talk_3");
+
+
+
     private void Awake()
     {
+        _animator = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
         _npcHeadLookAt = GetComponent<NPCHeadLookAt>();
     }
 
     public void Interact(Transform playerHeadTransform)
     {
-        Debug.Log("Interacting with NPC");
-        
         //ANimator Talk
-        
+        _animator.SetTrigger(GetRandomAnim());
         chatParentImage.SetActive(true);
         _npcHeadLookAt.LookAtPosition(playerHeadTransform);
         Talk();
@@ -33,6 +46,10 @@ public class NPCBehavior : MonoBehaviour, ITalkable
 
     private void Talk()
     {
+        _audioSource.PlayOneShot(talkClips[_currentChatIndex] != null
+            ? talkClips[_currentChatIndex]
+            : talkClips[Random.Range(0, talkClips.Length)]);
+        
         if (_currentChatIndex == chat.Length - 1)
         {
             //last chat
@@ -43,6 +60,7 @@ public class NPCBehavior : MonoBehaviour, ITalkable
             StartCoroutine(RestartGame());
             return;
         }
+
         _typingCoroutine = StartCoroutine(TypeSentence(chat[_currentChatIndex]));
         _currentChatIndex++;
     }
@@ -62,6 +80,21 @@ public class NPCBehavior : MonoBehaviour, ITalkable
         }
         
         _isTyping = false;
+    }
+    private int GetRandomAnim()
+    {
+        int randomIndex = Random.Range(0, 3);
+        switch (randomIndex)
+        {
+            case 0:
+                return TalkAnim_1;
+            case 1:
+                return TalkAnim_2;
+            case 2:
+                return TalkAnim_3;
+            default:
+                return TalkAnim_1;
+        }
     }
     private IEnumerator RestartGame()
     {
